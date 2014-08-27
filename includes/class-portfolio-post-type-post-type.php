f45130cc8738646b7d8520307a323dc358b9e655
Devin Price <devin@wptheming.com>
2014-08-27 11:12:16 -0500

    Fix broken image in WP3.7.  Update readme.

--- includes/class-portfolio-post-type-post-type.php
+++ includes/class-portfolio-post-type-post-type.php
@@ -70,7 +70,7 @@ class Portfolio_Post_Type_Post_Type extends Gamajo_Post_Type {
 			'capability_type' => 'post',
 			'rewrite'         => array( 'slug' => 'portfolio', ), // Permalinks format
 			'menu_position'   => 5,
-			'menu_icon'       => 'dashicons-portfolio',
+			'menu_icon'       => ( version_compare( $GLOBALS['wp_version'], '3.8', '>=' ) ) ? 'dashicons-portfolio' : false ,
 			'has_archive'     => true,
 		);
 
